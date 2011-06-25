dbf13b1962b5f338c4319ddd9b350a22a324d95d
edoo20 <emo.echee@yahoo.co.id>
2011-06-25 02:07:40 -0700

    Edited examples/example.php via GitHub

--- examples/example.php
+++ examples/example.php
@@ -1,20 +1,17 @@
 <?php
-/**
- * Copyright 2011 Facebook, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License. You may obtain
- * a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
+// Awesome Facebook Application
+//
+// Name: Bluetooth
+//
 
+require_once 'facebook-php-sdk/src/facebook.php';
+
+// Create our Application instance.
+$facebook = new Facebook(array(
+  'appId' => '114102938679814',
+  'secret' => '1a078d23c6bf15b13fd450010e358967',
+  'cookie' => true,
+)); 
 require '../src/facebook.php';
 
 // Create our Application instance (replace this with your appId and secret).
